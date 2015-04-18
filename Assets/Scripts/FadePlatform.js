e2737730904f73bae6ac4d72ebe8cf5a6d92e952
Nick Yeung <dmsnick88@gmail.com>
2015-04-18 16:11:37 -0400

    added some scripts

---
+++ Assets/Scripts/FadePlatform.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var target  : GameObject;
+private var stepped = false;
+
+function OnTriggerEnter (hit : Collider) {
+	stepped = true;
+}
+
+function OnTriggerStay (hit : Collider){
+	iTween.FadeTo(target,0,2);
+	if (stepped) {
+		if(target.GetComponent.<Renderer>().material.color.a == 0) GetComponent(TrigDestroy).Die(target);
+	}
+	print (target.GetComponent.<Renderer>().material.color.a);
+}
+
+function OnTriggerExit (hit : Collider){
+	
+}
+
+/*function die(){
+	Destroy(target);
+}*/
+
+function  die() {
+	GetComponent(TrigDestroy).Die(target);
+}
---
+++ Assets/Scripts/TrigDestroy.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function Die(target : GameObject) {
+	Destroy(target);
+}
