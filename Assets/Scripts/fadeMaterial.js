aeb760920f6daf29ee68ed51c637e97040e9c3e5
Nick Yeung <dmsnick88@gmail.com>
2015-05-10 02:54:58 -0400

    added more shtuff

--- Assets/Scripts/fadeMaterial.js
+++ Assets/Scripts/fadeMaterial.js
@@ -1,9 +1,19 @@
-﻿#pragma strict
+﻿var material1 : Material;
+var material2 : Material;
+var duration = 2.0;
+var rend: Renderer;
+
 
 function Start () {
-
+	rend = GetComponent.<Renderer>();
+	
+	// At start, use the first material
+	rend.material = material1;
 }
 
-function Update () {
 
+function Update () {
+	// ping-pong between the materials over the duration
+	var lerp : float = Mathf.PingPong(Time.time, duration) / duration;
+	rend.material.Lerp(material1, material2, lerp);
 }
---
+++ Assets/Scripts/fadetoBlackscreen.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var fadeSpeed : float = 1.5;  
+
+function FadeToBlack ()
+{
+    // Lerp the colour of the texture between itself and black.
+    GetComponent.<GUITexture>().color = Color.Lerp(GetComponent.<GUITexture>().color, Color.black, fadeSpeed * Time.deltaTime);
+}
+function Update () {
+	FadeToBlack();
+}
