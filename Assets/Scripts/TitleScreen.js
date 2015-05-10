dedfbcc26b5ca34f170a27091c07f44247d36264
Nick Yeung <dmsnick88@gmail.com>
2015-05-10 12:44:48 -0400

    finished menu screen

---
+++ Assets/Scripts/TitleScreen.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var prefab : GameObject;
+var FadeBackground : GameObject;
+var cam : Camera;
+private var isStarted : boolean = false;
+
+function Start () {
+	prefab.GetComponent(TrailRenderer).enabled = false;
+}
+
+function Update () {
+	if (!isStarted) {
+		if (Input.anyKey) {
+			prefab.GetComponent(TrailRenderer).enabled = true;
+			FadeBackground.GetComponent(fadetoBlackscreen).enabled = true;
+			cam.transform.Rotate(Vector3(45*Time.deltaTime,0,0));
+			if (cam.transform.eulerAngles.x >= 180) isStarted = true;
+		}
+	}
+	if (isStarted) GetComponent(ResetLevel).Reset();
+	
+
+}
